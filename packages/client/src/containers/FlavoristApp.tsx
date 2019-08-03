5a05731920ce960780e3bb363d565cf7cbf48958
James Canning <james@brudil.com>
2019-08-03 12:47:59 +0100

    Add code splitting
    Closes #18

--- packages/client/src/containers/FlavoristApp.tsx
+++ packages/client/src/containers/FlavoristApp.tsx
@@ -1,18 +1,47 @@
 import React from 'react';
 import { Sidebar } from '../components/Sidebar';
 import { Router } from '@reach/router';
-import { Explore } from './Explore';
-import { Home } from './Home';
-import { BatchesRoot } from './BatchesRoot';
-import { User } from './User';
-import { FourOhFour } from './FourOhFour';
 import { useQuery } from '@apollo/react-hooks';
 import gql from 'graphql-tag';
 import { auth } from '../context/authentication';
-import { Login } from './Login';
-import { Register } from './Register';
 import { Global } from '@emotion/core';
 import Helmet from 'react-helmet';
+import loadable from '@loadable/component';
+
+const Home = loadable(async () => {
+  const { Home } = await import('./Home');
+  return Home;
+});
+
+const Explore = loadable(async () => {
+  const { Explore } = await import('./Explore');
+  return Explore;
+});
+
+const FourOhFour = loadable(async () => {
+  const { FourOhFour } = await import('./FourOhFour');
+  return FourOhFour;
+});
+
+const Login = loadable(async () => {
+  const { Login } = await import('./Login');
+  return Login;
+});
+
+const Register = loadable(async () => {
+  const { Register } = await import('./Register');
+  return Register;
+});
+
+const User = loadable(async () => {
+  const { User } = await import('./User');
+  return User;
+});
+
+const BatchesRoot = loadable(async () => {
+  const { BatchesRoot } = await import('./BatchesRoot');
+  return BatchesRoot;
+});
 
 export const FlavoristApp: React.FC = () => {
   const { data, loading } = useQuery(gql`
