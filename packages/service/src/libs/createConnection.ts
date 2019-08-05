2a48e53479ba15b70cd9bc976256428f498f68b4
James Canning <james@brudil.com>
2019-08-05 23:16:52 +0100

    Add createConnection helper for pagination/connections

--- packages/service/src/libs/createConnection.ts
+++ packages/service/src/libs/createConnection.ts
@@ -1,9 +1,50 @@
-export function createConnection<E>(items: E[], { first, last, cursor }) {
+import { decode, encode } from './base64';
+
+interface ConnectionArgs {
+  cursor?: string | null;
+  first?: number | null;
+  last?: number | null;
+}
+
+const encodeCursor = (id: number, offset: number) => encode(`${id}/${offset}`);
+const decodeCursor = (cursor: string) => {
+  const parts = decode(cursor).split('/');
+
   return {
-    hasNextPage: false,
-    hasPreviousPage: false,
-    edges: items.map((item) => ({ node: item })),
-    startCursor: 'XXX',
-    endCursor: 'XXX',
+    id: parseInt(parts[0], 10),
+    offset: parseInt(parts[1], 10),
+  };
+};
+
+export function createConnection({ first, last, cursor }: ConnectionArgs) {
+  const previousCursor = cursor ? decodeCursor(cursor) : { offset: 0 };
+
+  const take = (first || last || 30) + 1;
+
+  return {
+    payload<E>(items: (E & { id: number })[]) {
+      const edges = items.map((item, index) => ({
+        node: item,
+        cursor: encodeCursor(item.id, previousCursor.offset + index + 1),
+      }));
+
+      const hasNextPage = edges.length > take;
+
+      return {
+        edges: hasNextPage ? edges.slice(0, -1) : edges,
+        pageInfo: {
+          hasNextPage,
+          hasPreviousPage: previousCursor.offset > 0,
+          startCursor: edges[0].cursor,
+          endCursor: edges[items.length - 1].cursor,
+        },
+      };
+    },
+    args(): { take?: number; skip: number } {
+      return {
+        take,
+        skip: previousCursor.offset,
+      };
+    },
   };
 }
--- packages/service/src/query/ingredients/index.ts
+++ packages/service/src/query/ingredients/index.ts
@@ -1,13 +1,23 @@
 import { Resolvers } from '../../generated/graphql';
 import { getRepository } from 'typeorm';
 import { Ingredient } from '../../entity/Ingredient';
+import { createConnection } from '../../libs/createConnection';
 
 export const ingredientQuery: Resolvers = {
   Query: {
-    allIngredients: async (_parent) => {
+    allIngredients: async (_parent, { cursor, first, last }) => {
       const ingredientRepo = getRepository(Ingredient);
 
-      return await ingredientRepo.find({ relations: ['vendor'], take: 100 });
+      const connection = createConnection({ first, cursor, last });
+
+      console.log(connection.args());
+      const ingredients = await ingredientRepo.find({
+        relations: ['vendor'],
+        order: { name: 'ASC' },
+        ...connection.args(),
+      });
+
+      return connection.payload(ingredients);
     },
   },
   Ingredient: {},
