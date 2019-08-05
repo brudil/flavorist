a13e961ed87545f9011bdd1f5a5372ee8878b094
James Canning <james@brudil.com>
2019-08-05 17:41:23 +0100

    Add simple infuser import command for dev

---
+++ packages/service/src/cli/importFlavors.ts
@@ -0,0 +1,45 @@
+import 'reflect-metadata';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+import { promises as fs } from 'fs';
+import * as path from 'path';
+import { setupDb } from '../db';
+import { getRepository } from 'typeorm';
+import { Flavor } from '../entity/Flavor';
+import { Vendor } from '../entity/Vendor';
+
+(async () => {
+  const connection = await setupDb();
+
+  const flavorRepo = getRepository(Flavor);
+  const vendorRepo = getRepository(Vendor);
+
+  const file = await fs.readFile(
+    path.resolve(process.cwd(), '../infuser/flavors.generated.json'),
+    { encoding: 'utf-8' },
+  );
+
+  const data: any[] = JSON.parse(file);
+
+  for (const flavor of data) {
+    console.log(flavor);
+
+    let vendor = await vendorRepo.findOne({ shortName: flavor.vendor });
+    if (!vendor) {
+      vendor = new Vendor();
+      vendor.name = flavor.vendor;
+      vendor.shortName = flavor.vendor;
+
+      await vendorRepo.save(vendor);
+    }
+
+    const flavorEntity = new Flavor();
+    flavorEntity.name = flavor.name;
+    flavorEntity.vendor = vendor;
+
+    await flavorRepo.save(flavorEntity);
+  }
+
+  await connection.close();
+})();
