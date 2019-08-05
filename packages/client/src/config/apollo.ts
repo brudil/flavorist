59b1838f6fa3ac90a225c2b1bd91cd6c071ba45e
James Canning <james@brudil.com>
2019-08-05 17:38:12 +0100

    Add ApolloKeyMapper lib for multi-ID support

--- packages/client/src/config/apollo.ts
+++ packages/client/src/config/apollo.ts
@@ -2,24 +2,46 @@ import { createHttpLink } from 'apollo-link-http';
 import { ApolloClient } from 'apollo-client';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import { persistCache } from 'apollo-cache-persist';
+import { createApolloKeyMapper } from '../libs/apolloKeyMapper';
 
 export async function setupApolloClient() {
+  const keyMap = createApolloKeyMapper({
+    User: [['username']],
+    Recipe: [['username', 'slug']],
+    Vendor: [['shortName']],
+  });
+
   const link = createHttpLink({
     uri: '/graphql',
     credentials: 'same-origin',
   });
 
   const cache = new InMemoryCache({
-    dataIdFromObject: (object) => object.id,
+    dataIdFromObject: (object) => {
+      return object.id;
+    },
+    cacheRedirects: {
+      Query: {
+        user(_, obj) {
+          return keyMap.try(obj);
+        },
+      },
+    },
   });
 
-  await persistCache({
-    cache,
-    storage: window.localStorage as any,
-  });
+  keyMap.init(cache);
 
-  return new ApolloClient({
+  if (process.env.NODE_ENV === 'production') {
+    await persistCache({
+      cache,
+      storage: window.localStorage as any,
+    });
+  }
+
+  const client = new ApolloClient({
     cache,
     link,
   });
+
+  return client;
 }
---
+++ packages/client/src/libs/apolloKeyMapper.ts
@@ -0,0 +1,95 @@
+// Apollo Client 3.0 should have built in support for multiple IDs for entities within the cache
+// this is a /bad/ stopgap
+
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { isObject } from 'lodash';
+// import {toIdValue} from "apollo-utilities";
+// @ts-ignore
+import { rIC } from 'idlize/idle-callback-polyfills.mjs';
+
+interface Config {
+  [typename: string]: string[][];
+}
+
+export const createApolloKeyMapper = (config: Config) => {
+  const keyMap = new Map<string, string>();
+
+  const runForEntity = (entity: any) => {
+    if (!config.hasOwnProperty(entity.__typename)) {
+      return;
+    }
+
+    const configForTypename = config[entity.__typename];
+
+    for (const configOption of configForTypename) {
+      const missingFields = configOption.reduce(
+        (prev, field) => prev || !entity.hasOwnProperty(field),
+        false,
+      );
+
+      if (!missingFields) {
+        const keyValues = configOption.map((key) => entity[key]);
+        keyMap.set(
+          `${entity.__typename}:${configOption.join('&')}:${keyValues.join(
+            '&',
+          )}`,
+          entity.id,
+        );
+      }
+    }
+  };
+
+  return {
+    try(value: any) {
+      console.log('attempting from keyMap', value);
+      // const key = `User:username:${obj.username}`;
+      // console.log('cache redirect', obj, key);
+      //
+      // if (obj.hasOwnProperty('username')) {
+      //     if (keyMap.hasOwnProperty(key)) {
+      //         console.log(keyMap[key]);
+      //         return toIdValue(keyMap[key]);
+      //     }
+      // }
+    },
+
+    init(cache: InMemoryCache) {
+      // wrap the cache's write method with our own
+      const write = cache.write;
+      cache.write = (...args: any) => {
+        write.apply(cache, args);
+
+        try {
+          const perform = (obj: any) => {
+            if (obj.hasOwnProperty('__typename')) {
+              runForEntity(obj);
+            }
+
+            for (const key in obj) {
+              if (!obj.hasOwnProperty(key)) {
+                continue;
+              }
+
+              if (isObject(obj[key])) {
+                perform(obj[key]);
+              }
+            }
+          };
+
+          rIC(
+            () => {
+              Object.values(args[0].result).forEach((value: any) => {
+                perform(value);
+              });
+
+              console.log('write to cache!', keyMap);
+            },
+            { timeout: 400 },
+          );
+        } catch (e) {
+          console.warn(e);
+        }
+      };
+    },
+  };
+};
