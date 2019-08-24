3c0c4f4be06bdd5009bac85db5afe9a75e31f355
James Canning <james@brudil.com>
2019-08-24 17:57:26 +0100

    Fix query typo

--- packages/client/src/containers/RecipesLibrary.tsx
+++ packages/client/src/containers/RecipesLibrary.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link, RouteComponentProps } from '@reach/router';
 import { useQuery } from '@apollo/react-hooks';
-import { getViewerRecipes } from '../graphql/queries/viewer/getVIewerRecipes';
+import { getViewerRecipes } from '../graphql/queries/viewer/getViewerRecipes';
 import { GetViewerRecipesQuery } from '../generated/graphql';
 
 export const RecipesLibrary: React.FC<RouteComponentProps> = () => {
