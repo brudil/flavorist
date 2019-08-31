d4f94af5afcd5347ae8ad66375c3ea89c08031b4
James Canning <james@brudil.com>
2019-08-31 01:42:04 +0100

    Start styling of ingredients/simple table/listing

---
+++ packages/client/src/components/IngredientTable.tsx
@@ -0,0 +1,84 @@
+import React from 'react';
+import { type, TypeSize } from '../style/type';
+
+export const IngredientTable: React.FC<React.HTMLProps<HTMLTableElement>> = ({
+  children,
+  ...props
+}) => {
+  return (
+    <table
+      {...props}
+      css={{
+        width: '100%',
+        borderCollapse: 'collapse',
+      }}
+    >
+      {children}
+    </table>
+  );
+};
+
+export const IngredientTableRow: React.FC<
+  React.HTMLProps<HTMLTableRowElement>
+> = ({ children, ...props }) => {
+  return (
+    <tr
+      {...props}
+      css={{
+        backgroundColor: '#fafafa',
+        marginBottom: '0.3rem',
+        borderBottom: '6px #fff solid',
+      }}
+    >
+      {children}
+    </tr>
+  );
+};
+
+export enum IngredientInventoryStatus {
+  Available,
+  RunningLow,
+  Absent,
+  Header,
+}
+
+const inventoryStatusColorMap = {
+  [IngredientInventoryStatus.Available]: '#22ff22',
+  [IngredientInventoryStatus.RunningLow]: '#ffb125',
+  [IngredientInventoryStatus.Absent]: '#ff3526',
+  [IngredientInventoryStatus.Header]: 'transparent',
+};
+
+export const IngredientTableIndicator: React.FC<{
+  ingredientStatus: IngredientInventoryStatus;
+}> = ({ ingredientStatus }) => {
+  return (
+    <td
+      css={{
+        width: 3,
+        background: inventoryStatusColorMap[ingredientStatus],
+      }}
+    />
+  );
+};
+
+export const IngredientTableCell: React.FC<
+  React.HTMLProps<HTMLTableCellElement>
+> = ({ children, ...props }) => {
+  return (
+    <td
+      {...props}
+      css={{
+        padding: '0.5rem',
+        fontWeight: 600,
+        ...type(TypeSize.Pica),
+        '& a': {
+          color: '#333',
+          textDecoration: 'none',
+        },
+      }}
+    >
+      {children}
+    </td>
+  );
+};
--- packages/client/src/containers/Ingredients.tsx
+++ packages/client/src/containers/Ingredients.tsx
@@ -5,6 +5,14 @@ import { useQuery } from '@apollo/react-hooks';
 import { getIngredients } from '../graphql/queries/ingredients/getIngredients';
 import { GetIngredientsQuery } from '../generated/graphql';
 import { paginationFetchMore } from '../libs/simplePagninationUpdateQuery';
+import {
+  IngredientInventoryStatus,
+  IngredientTable,
+  IngredientTableCell,
+  IngredientTableIndicator,
+  IngredientTableRow,
+} from '../components/IngredientTable';
+import { ViewContainer } from '../components/ViewContainer';
 
 export const Ingredients: React.FC<RouteComponentProps> = () => {
   const { data, error, loading, fetchMore } = useQuery<GetIngredientsQuery>(
@@ -12,35 +20,58 @@ export const Ingredients: React.FC<RouteComponentProps> = () => {
   );
 
   return (
-    <div>
+    <ViewContainer>
       <Helmet title="Ingredients" />
       <h1>Explore Ingredients</h1>
 
-      <ul>
-        {(!loading || error) &&
-        data &&
-        data.allIngredients &&
-        data.allIngredients.edges
-          ? data.allIngredients.edges.map(
-              (ingredient) =>
-                ingredient &&
-                ingredient.node && (
-                  <li key={ingredient.node.id}>
-                    <Link
-                      to={`/ingredients/${ingredient.node.vendor &&
-                        ingredient.node.vendor.shortName}/${
-                        ingredient.node.slug
-                      }`}
-                    >
-                      {ingredient.node.vendor &&
-                        ingredient.node.vendor.shortName}{' '}
-                      {ingredient.node.name}
-                    </Link>
-                  </li>
-                ),
-            )
-          : null}
-      </ul>
+      <IngredientTable>
+        <thead>
+          <IngredientTableIndicator
+            ingredientStatus={IngredientInventoryStatus.Header}
+          />
+          <td>Vendor</td>
+          <td>Name</td>
+          <td>Average used</td>
+        </thead>
+        <tbody>
+          {(!loading || error) &&
+          data &&
+          data.allIngredients &&
+          data.allIngredients.edges
+            ? data.allIngredients.edges.map(
+                (ingredient) =>
+                  ingredient &&
+                  ingredient.node && (
+                    <IngredientTableRow key={ingredient.node.id}>
+                      <IngredientTableIndicator
+                        ingredientStatus={IngredientInventoryStatus.Absent}
+                      />
+                      <IngredientTableCell css={{ '& a': { color: '#888' } }}>
+                        <Link
+                          to={`/vendor/${ingredient.node.vendor &&
+                            ingredient.node.vendor.shortName}`}
+                        >
+                          {ingredient.node.vendor &&
+                            ingredient.node.vendor.shortName}
+                        </Link>
+                      </IngredientTableCell>
+                      <IngredientTableCell>
+                        <Link
+                          to={`/ingredients/${ingredient.node.vendor &&
+                            ingredient.node.vendor.shortName}/${
+                            ingredient.node.slug
+                          }`}
+                        >
+                          {ingredient.node.name}
+                        </Link>
+                      </IngredientTableCell>
+                      <IngredientTableCell>tbi</IngredientTableCell>
+                    </IngredientTableRow>
+                  ),
+              )
+            : null}
+        </tbody>
+      </IngredientTable>
 
       <button
         onClick={() =>
@@ -54,6 +85,6 @@ export const Ingredients: React.FC<RouteComponentProps> = () => {
       >
         Load more
       </button>
-    </div>
+    </ViewContainer>
   );
 };
